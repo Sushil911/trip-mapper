e4996210cce6978331b4f73e26b47c130c9a0437
Sushil911 <Sushilbasyal30@gmail.com>
2025-11-01 01:02:16 +0545

    add signin and signup view

--- src/components/sign-up-view.tsx
+++ src/components/sign-up-view.tsx
@@ -217,7 +217,7 @@ export const SignUpView = () => {
 
                 <div className="bg-radial from-sidebar-accent to-sidebar relative hidden md:flex flex-col gap-y-4 justify-center items-center">
                     <img src='/globe.svg' alt="logo" className="h-[92px] w-[92px]" />
-                    <p className="text-2xl font-semibold text-white">
+                    <p className="text-2xl font-semibold text-black">
                         Tripmapper
                     </p>
                 </div>
